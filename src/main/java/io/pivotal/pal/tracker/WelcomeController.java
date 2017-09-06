ddcca1c33664abe999213cda9f30e97ddb26495d
sowjanya-m <sowjanyamulamreddy@gmail.com>
2017-09-06 07:51:38 +0530

    Deploy a Spring Boot app to Cloud Foundry

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -2,12 +2,21 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.*;
 
 @RestController
 public class WelcomeController {
+    private String name;
 
+    WelcomeController() {
+    }
+
+    @Autowired
+    public WelcomeController(@Value("${welcome_message:NOT SET}") String name){
+        this.name = name;
+    }
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return name;
     }
 }
